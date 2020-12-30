bd7f03d86679ba772e83b3d02ea175a8c6b68e62
Drew Dickerson <ddickerson@magmutual.com>
2020-12-30 15:58:18 -0500

    App initialization in the package

--- googleplex_assistant/__init__.py
+++ googleplex_assistant/__init__.py
@@ -4,10 +4,15 @@ from flask import Flask
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    from googleplex_assistant.controllers import hello, config
+    from googleplex_assistant.controllers import hello, config, ifttt
     app.register_blueprint(hello.bp)
     app.register_blueprint(config.bp)
+    app.register_blueprint(ifttt.bp)
 
     from googleplex_assistant.helpers import upnpclient
     upnpclient.init_app()
+    upnpclient.map_port()
     return app
+
+
+application = create_app()
