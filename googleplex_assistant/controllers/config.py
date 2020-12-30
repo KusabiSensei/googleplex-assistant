78b41917f6dc69ec13a6aa0b449ef253149aa6bc
Drew Dickerson <ddickerson@magmutual.com>
2020-12-30 15:57:50 -0500

    Add the Chromecasts endpoint

--- googleplex_assistant/controllers/config.py
+++ googleplex_assistant/controllers/config.py
@@ -8,3 +8,8 @@ bp = Blueprint('config', __name__, url_prefix='/config')
 def dump_config():
     external_ip_config = upnpclient.map_port()
     return {'external_ip_configuration': external_ip_config}
+
+
+@bp.route('/chromecasts')
+def dump_chromecasts():
+    return {'chromecasts': 'List of Chromecasts will be here'}
