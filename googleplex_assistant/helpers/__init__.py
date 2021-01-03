9922d402fec83d07c577265fdc6c4bce1e46617f
Drew Dickerson <ddickerson@magmutual.com>
2021-01-03 15:35:40 -0500

    Do some initial discovery for Chromecasts on the local network.

--- googleplex_assistant/helpers/__init__.py
+++ googleplex_assistant/helpers/__init__.py
@@ -1,9 +1,19 @@
 import logging
 import miniupnpc
+from pychromecast import get_chromecasts
+from .plex import server as plex_server
+
+
+
+def cc_discovery_callback(chromecast):
+    chromecasts[chromecast.device.friendly_name] = chromecast
+
 
 logger = logging.getLogger(__name__)
-logger.warning("Now initializing helper objects...")
+logger.info("Now initializing helper objects...")
+chromecasts = {}
 upnp_client = miniupnpc.UPnP()
 devices = upnp_client.discover()
 gateway = upnp_client.selectigd()
+get_chromecasts(blocking=False, callback=cc_discovery_callback)
 
